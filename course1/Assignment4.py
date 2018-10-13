aaaa51ea5d90ae189276e6fd7710e8154d3464ad
Neil <denck007@umn.edu>
2018-10-13 14:01:47 -0500

    added contract

--- course1/Assignment4.py
+++ course1/Assignment4.py
@@ -19,7 +19,7 @@ Initially it is best to implement the contraction alorithm niavely by creating a
 def build_adjacency_list(input):
     '''
     input is a list of lists
-    returns vertex_edge and edge_vertex lists
+    returns vertex_edges and edge_verticies lists
     input: 
         First item in each row is the vertex id
         rest of items in each row are connected verticies
@@ -29,11 +29,11 @@ def build_adjacency_list(input):
         edge_verticies:
             edge_verticies[m_jj] are the [start,end] verticies of edge m_jj
     '''
-    num_verticies = input[-1][0] +1
+    num_verticies = input[-1][0]-1
     edge_verticies = build_edge_verticies(input)
     vertex_edges = build_vertex_edges(edge_verticies,num_verticies)
 
-    return edge_verticies,vertex_edges
+    return vertex_edges,edge_verticies
 
 def build_edge_verticies(input):
     '''
@@ -73,6 +73,35 @@ def build_vertex_edges(edge_verticies,num_verticies):
             vertex_edges[vertex].append(jj)
     return vertex_edges
 
+def remove_self_loops(vertex_edges,edge_verticies,valid_edges):
+    '''
+    Remove references to self loops in vertex_edges and edge_verticies
+    This function does not renumber the edges, it simply removes the self loops
+        This means that there care edges whose verticie list is empty, ie edge_vertices[jj] == []
+    Returns the lists
+    '''
+    # in a self loop the same edge is listed 2 times for the same vertex
+    for ii in range(len(vertex_edges)):
+        vertex_edges[ii] = list(set(vertex_edges[ii]))
+
+    for edge in valid_edges[::-1]:
+        if edge_verticies[edge][0] == edge_verticies[edge][1]:  #is self loop
+            edge_verticies[edge] = []
+            valid_edges.remove(edge)
+    return vertex_edges,edge_verticies,valid_edges
+
+def contract_graph(vertex_edges,edge_verticies,edge):
+    '''
+    Contract a graph by removing the specified edge via merging the corresponding verticies
+    '''
+    # remove reference to edge in vertex_edges
+    vertex_edges[edge_verticies[edge][0]].remove(edge)
+    vertex_edges[edge_verticies[edge][1]].remove(edge)
+    edge_verticies[edge] = []
+
+    return vertex_edges,edge_verticies
+
+
 input = [[1,2,3,4],
         [2,1,4],
         [3,1,4],
@@ -89,9 +118,18 @@ edge_verticies_true = [[],
                     [1,4],
                     [2,4],
                     [3,4]]
-edge_verticies,vertex_edges = build_adjacency_list(input)
+vertex_edges,edge_verticies = build_adjacency_list(input)
 assert edge_verticies == edge_verticies_true,"Failed edge_verticies\n\t{}\n\t{}".format(edge_verticies,edge_verticies_true)
 assert vertex_edges == vertex_edges_true, "Failed vertex_edges\n\t{}\n\t{}".format(vertex_edges,vertex_edges_true)
+
+import random
+valid_edges = [x for x in range(1,len(edge_verticies))]
+while len(valid_edges) > 2:
+    edge = random.choice(valid_edges)
+    vertex_edges,edge_verticies = contract_graph(vertex_edges,edge_verticies,edge)
+    valid_edges.remove(edge)
+
+    vertex_edges,edge_verticies,valid_edges = remove_self_loops(vertex_edges,edge_verticies,valid_edges)
 
 
 
@@ -121,9 +159,8 @@ edge_verticies_true = [[],
                     [3,6],
                     [4,6],
                     [6,6]]
-edge_verticies,vertex_edges = build_adjacency_list(input)
+vertex_edges,edge_verticies = build_adjacency_list(input)
 assert edge_verticies == edge_verticies_true,"Failed edge_verticies\n\t{}\n\t{}".format(edge_verticies,edge_verticies_true)
 assert vertex_edges == vertex_edges_true, "Failed vertex_edges\n\t{}\n\t{}".format(vertex_edges,vertex_edges_true)
-
 
 print("Passed test")
