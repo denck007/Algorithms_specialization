784fe77a0f1355e2bf62e821a4431cab8aab0dd2
Neil Dencklau <denck007@umn.edu>
2018-10-31 15:38:31 -0500

    working solution

--- course2/assignment2.py
+++ course2/assignment2.py
@@ -34,13 +34,15 @@ class Graph():
             for line in f.readlines():
                 if line[0] == "#":
                     continue
-                line_split = line.split("\t")
+                line_split = line.strip("\n").split("\t")
 
                 vertex = int(line_split[0])
                 self.num_verticies += 1
                 self.vertex_edges.update({vertex:[]})
                 for edge in line_split[1:]:
                     edge_split = edge.strip("\n").split(",")
+                    if edge_split == [""]:
+                        continue
                     self.num_edges+=1
                     self.vertex_edges[vertex].append(self.num_edges)
                     self.edge_verticies.update({self.num_edges:[vertex,int(edge_split[0])]})
@@ -80,7 +82,7 @@ class Heap():
         '''
         num_verticies are the total number of verticies in the graph
         '''
-        self.null_cost = int(1e5) # theoretical max value
+        self.null_cost = int(1e10) # theoretical max value
         
         self.num_verticies = num_verticies
         # the max number of items that we can ever see is the number of verticies in the graph
@@ -100,6 +102,7 @@ class Heap():
         '''
         vertex = self.heap_vertex[0]
         length = self.delete(vertex)
+        #self.check_heap("extract_min")
         return (vertex,length)
 
     def insert(self,vertex,length):
@@ -109,8 +112,10 @@ class Heap():
         self.heap[self.end_of_heap] = length
         self.heap_vertex[self.end_of_heap] = vertex
         self.vertex_heap[vertex] = self.end_of_heap
-        self._bubble_up(self.end_of_heap)
+        if self.end_of_heap > 0:
+            self._bubble_up(self.end_of_heap)
         self.end_of_heap += 1
+        #self.check_heap("insert")
 
     def delete(self,vertex):
         '''
@@ -119,12 +124,13 @@ class Heap():
         '''
         heap_idx = self.vertex_heap[vertex]
         length = self.heap[heap_idx]
-        
+
         self._swap(heap_idx,self.end_of_heap-1) #move value to be deleted to end of array
         self.end_of_heap -= 1 # move the end of the heap marker
-        self.heap_vertex[self.end_of_heap] = -9 # remove all reference to it
-        self.heap[self.end_of_heap] = self.null_cost+1
+        self.heap[self.end_of_heap] = self.null_cost+3
         self._bubble_down(heap_idx)
+        self._bubble_up(heap_idx)
+        #self.check_heap("delete")
 
         return length
 
@@ -132,8 +138,8 @@ class Heap():
         '''
         maintain the invariant that all parents are <= children
         '''
-        parent_idx = change_idx//2
-        if self.heap[parent_idx] > self.heap[change_idx]:
+        parent_idx = (change_idx+1)//2 - 1# +1 is for 0 based indexing in self.heap
+        if (self.heap[parent_idx] > self.heap[change_idx]) and (change_idx > 0):
             self._swap(parent_idx,change_idx)
             self._bubble_up(parent_idx)
 
@@ -158,10 +164,10 @@ class Heap():
             right_length = self.null_cost
 
         # the second comparision in each if statement is the base case, ie we hit the end of the heap
-        if (self.heap[change_idx] < left_length) and (left_idx < self.end_of_heap): 
+        if (self.heap[change_idx] > left_length) and (left_idx < self.end_of_heap): 
             self._swap(change_idx,left_idx)
             self._bubble_down(left_idx)
-        elif (self.heap[change_idx] < right_length) and (right_idx < self.end_of_heap):
+        elif (self.heap[change_idx] > right_length) and (right_idx < self.end_of_heap):
             self._swap(change_idx,right_idx)
             self._bubble_down(right_idx)
 
@@ -184,17 +190,23 @@ class Heap():
         self.heap_vertex[destination_idx] = tmp
 
     def __len__(self):
+        '''
+        return how many items are on the heap
+        '''
         return self.end_of_heap
 
-    def force_validate(self):
+    def check_heap(self,call_from):
         '''
-        Go through the tree vertex by vertex and verify that every child is greater or equal to its parent
+        Go through the whole graph and make sure all children are >= parents
         '''
-        
-
-
-
-
+        for ii in range(self.end_of_heap):
+            left_idx = ii*2+1
+            right_idx = ii*2+2
+            if left_idx < self.end_of_heap:
+                assert self.heap[left_idx] >= self.heap[ii],'{} Left failed at idx {} with {} and {}'.format(call_from,ii,self.heap[left_idx],self.heap[ii])
+            if right_idx < self.end_of_heap:
+                assert self.heap[right_idx] >= self.heap[ii],'{} Right failed at idx {} with {} and {}'.format(call_from,ii,self.heap[right_idx],self.heap[ii])
+        print("{} heap test passed".format(call_from))
 
 def add_to_heap(h,g,tail,explored,added_to_heap_by):
     '''
@@ -211,49 +223,73 @@ def add_to_heap(h,g,tail,explored,added_to_heap_by):
     for edge in g.vertex_edges[tail]:
         head = g.edge_verticies[edge][1]
         if not explored[head]:
-            if head == 7:
-                print("vertex7")
-            length = g.edge_lengths[edge]
+            proposed_length = g.shortest_path[tail] + g.edge_lengths[edge]
 
             # if the head is already on the heap, added_to_heap_by[head]==-1 means it has not been added yet
             if added_to_heap_by[head] >= 0:
                 previous_length = h.delete(head)
-                if length > previous_length:
-                    length = previous_length
-                    #added_to_heap_by[head] = added_to_heap_by[head] no need to update this
+                if proposed_length > previous_length:
+                    new_length = previous_length
                 else:
-                    #length = length no need to update this
+                    new_length = proposed_length
                     added_to_heap_by[head] = tail
             else:
+                new_length = proposed_length
                 added_to_heap_by[head] = tail
-            h.insert(head,length)
+
+            h.insert(head,new_length)
 
     return added_to_heap_by
 
+def Dijkstra(fname,testing=False):
+    '''
+    run Dijkstra's shortest path alogrithm on the graph defined in fname
+    '''
+    G = Graph(fname,testing=testing)
+    G.initialize_shortest_path(1)
+    explored = [False for n in range(G.num_verticies+1)]
+    added_to_heap_by = [-1 for n in range(G.num_verticies+1)]
+    explored[1] = True
+
+    h = Heap(num_verticies=G.num_verticies)
+    added_to_heap_by = add_to_heap(h,G,1,explored,added_to_heap_by)
+    while len(h) > 0:
+        w,length = h.extract_min()
+        G.shortest_path.update({w:length})
+
+        path_to_current = G.paths[added_to_heap_by[w]].copy()
+        path_to_current.append(w)
+        G.paths.update({w:path_to_current})
+
+        explored[w] = True
+        added_to_heap_by = add_to_heap(h,G,w,explored,added_to_heap_by)
+
+    return G
+
 verticies_to_check = [7,37,59,82,99,115,133,165,188,197]
-G = Graph("course2/test_assignment2/input_random_1_4.txt",testing=True)
-G.initialize_shortest_path(1)
-explored = [False for n in range(G.num_verticies+1)]
-added_to_heap_by = [-1 for n in range(G.num_verticies+1)]
-explored[1] = True
+base_path = "course2/test_assignment2/"
+input_files = [os.path.join(base_path,f) for f in os.listdir(base_path) if "input" in f]
 
-h = Heap(num_verticies=G.num_verticies)
-added_to_heap_by = add_to_heap(h,G,1,explored,added_to_heap_by)
-while len(h) > 0:
-    w,length = h.extract_min()
-    G.shortest_path.update({w:G.shortest_path[added_to_heap_by[w]]+length})
-    print("{}: {}".format(w,length))
+for f in input_files:
+    if int(f[f.rfind("_")+1:-4]) > 15:
+        continue
+    print(f)
+    G = Dijkstra(f,testing=True)
 
-    path_to_current = G.paths[added_to_heap_by[w]].copy()
-    path_to_current.append(w)
-    G.paths.update({w:path_to_current})
+    for ii,vertex in enumerate(verticies_to_check):
+        if vertex in G.shortest_path.keys():
+            print("Vertex {}: {} == {}".format(vertex,G.shortest_path[vertex], G.true_output[ii]))
+        assert G.shortest_path[vertex] == G.true_output[ii]
 
-    explored[w] = True
-    added_to_heap_by = add_to_heap(h,G,w,explored,added_to_heap_by)
+    for ii,vertex in enumerate(verticies_to_check):
+        if vertex in G.shortest_path.keys():
+            print("Vertex {}: {} == {}\n".format(vertex,G.paths[vertex], G.true_paths[vertex]))
+        assert G.paths[vertex] == G.true_paths[vertex]
 
-for ii,vertex in enumerate(verticies_to_check):
-    #assert G.shortest_path[vertex] == G.output[ii]
-    print("Vertex {}: {} == {}".format(vertex,G.shortest_path[vertex], G.true_output[ii]))
 
-for ii,vertex in enumerate(verticies_to_check):
-    print("Vertex {}: {} == {}\n".format(vertex,G.paths[vertex], G.true_paths[vertex]))
+print("All tests passed!")
+
+input_files = [os.path.join(base_path,"input_random_4_4.txt")]
+G = Dijkstra("course2/assignment2_input.txt",testing=False)
+for v in verticies_to_check:
+    print(G.shortest_path[v])
